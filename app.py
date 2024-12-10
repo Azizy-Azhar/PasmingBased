6daca1b8643d313891bf18d7161c3536b6b35f84
Waldah Nurjamlah <Waldahnurjamilah354@gmail.com>
2024-12-10 16:42:01 +0700

    Mengupdate file app.py

--- app.py
+++ app.py
@@ -5,4 +5,11 @@ import pandas as pd
 import plotly.express as px
 
 
-st.title("Kelompok PasmingBased")
+st.write("## Visualisasi")
+st.write("Buat visualisasi yang menurut kelompok kalian perlu ditampilkan.")
+st.write("Gunakan juga elemen-elemen interaktif streamlit.")
+# Load the data
+data_path = "Happiness.csv"
+data = pd.read_csv(data_path, delimiter=';', skiprows=2)
+data = data.rename(columns={"Unnamed: 0": "Country"})
+data.set_index("Kabupaten/", inplace=True)
